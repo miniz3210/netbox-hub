3f42e9bd6f4c5a0c32257f3a88a87ccc3b969407
miniz3210 <ffxi.farseer@gmail.com>
2026-08-28 06:53:06 +0000

    fix(constants): add GITHUB_REPO and BRANCH aliases for catalog module

--- config/constants.py
+++ config/constants.py
@@ -6,9 +6,14 @@ APP_VERSION = "v2.4.0"
 APP_NAME = "NetBox Universal Library Hub"
 
 # GitHub Official Device-Type Library Source
-OFFICIAL_GITHUB_OWNER = "netbox-community"
-OFFICIAL_GITHUB_REPO = "devicetype-library"
-OFFICIAL_GITHUB_BRANCH = "master"
+OWNER = "netbox-community"
+REPO = "devicetype-library"
+BRANCH = "master"
+
+GITHUB_REPO = "netbox-community/devicetype-library"
+OFFICIAL_GITHUB_OWNER = OWNER
+OFFICIAL_GITHUB_REPO = REPO
+OFFICIAL_GITHUB_BRANCH = BRANCH
 
 # Data Storage
 DATABASE_PATH = "data/netbox_hub.db"
