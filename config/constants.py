8f33a5f99710f3e3861d4be1b39d710180b53483
miniz3210 <ffxi.farseer@gmail.com>
2026-08-28 07:16:35 +0000

    fix(ipam): fix preset order reload, capitalize branch names, and detect IN-USE subnets in NetBox DB

--- config/constants.py
+++ config/constants.py
@@ -2,7 +2,7 @@
 Application Constants and Versioning
 """
 
-APP_VERSION = "v2.4.0"
+APP_VERSION = "v2.4.2"
 APP_NAME = "NetBox Universal Library Hub"
 
 # GitHub Official Device-Type Library Source
