c36c31aea1857b3e6430bff5c83bdc9312731d48
miniz3210 <ffxi.farseer@gmail.com>
2026-08-28 06:32:31 +0000

    chore(release): bump version to v2.3.8

--- config/constants.py
+++ config/constants.py
@@ -1,4 +1,10 @@
-APP_VERSION = "v2.5.0"
-GITHUB_REPO = "netbox-community/devicetype-library"
-BRANCH = "master"
-RULES_FILE = "naming_rules.json"
+APP_VERSION = "v2.3.8"
+APP_NAME = "NetBox Universal Library Hub"
+
+# GitHub Official Device-Type Library Source
+OFFICIAL_GITHUB_OWNER = "netbox-community"
+OFFICIAL_GITHUB_REPO = "devicetype-library"
+OFFICIAL_GITHUB_BRANCH = "master"
+
+# Data Storage
+DATABASE_PATH = "data/netbox_hub.db"
