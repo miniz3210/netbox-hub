7ea12cfca7be16011fad542b2f13c71e787e9e35
miniz3210 <ffxi.farseer@gmail.com>
2026-08-28 06:35:14 +0000

    refactor(ui): update use_container_width to width='stretch' for modern Streamlit

--- config/constants.py
+++ config/constants.py
@@ -1,10 +1,4 @@
 APP_VERSION = "v2.3.8"
-APP_NAME = "NetBox Universal Library Hub"
-
-# GitHub Official Device-Type Library Source
-OFFICIAL_GITHUB_OWNER = "netbox-community"
-OFFICIAL_GITHUB_REPO = "devicetype-library"
-OFFICIAL_GITHUB_BRANCH = "master"
-
-# Data Storage
-DATABASE_PATH = "data/netbox_hub.db"
+GITHUB_REPO = "netbox-community/devicetype-library"
+BRANCH = "master"
+RULES_FILE = "naming_rules.json"
