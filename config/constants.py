785fa0c9002684422a254b6f854833d2074a7681
miniz3210 <ffxi.farseer@gmail.com>
2026-09-20 11:25:23 +0000

    bump v3.2.0.5

--- config/constants.py
+++ config/constants.py
@@ -2,7 +2,7 @@
 Application Constants and Versioning
 """
 
-APP_VERSION = "3.2.0.1"
+APP_VERSION = "3.2.0.5"
 APP_NAME = "NetBox Universal Library Hub"
 
 # GitHub Official Device-Type Library Source
